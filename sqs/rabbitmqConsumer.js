71de0e00b1d39616b558f357ea1c07131abc0f0a
xiaparrot <xiaparrot@163.com>
2025-08-21 11:52:21 +0800

    init

--- sqs/rabbitmqConsumer.js
+++ sqs/rabbitmqConsumer.js
@@ -69,14 +69,13 @@ async function consumeMessages() {
         let obj = JSON.parse(iotmsg)
         manageDeviceUpdateAccepted(obj).then(result => {
             console.log("RABBITMQ manageDeviceUpdateAccepted finish:"+iotmsg)
-            channel.ack(message);
         }).catch(async err => {
             if (err && err.message) {
                 console.log("RABBITMQ process error:"+err.message)
             }
         })}
     }, {
-      noAck: false // Important: Disable auto-acknowledgment
+      noAck: true // Important: Disable auto-acknowledgment
     });
 
     // Handle Connection Closure (Important)
